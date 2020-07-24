c18a6063e2b5c868046225206f3036170e58b1d1
goncmodev <gon.cmo.dev@gmail.com>
2020-07-24 14:12:36 -0400

    components and props creation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 
 function App() {
+
+  // get date
+  const date_now = new Date().getFullYear(); 
+
   return (
-    <div className="App">
-      <h1>Le Troupe</h1>
-    </div>
+    <Fragment>
+
+      <Header 
+        title = 'Productos Le Troupe'
+      />
+
+      <Footer 
+        date_now = {date_now}
+      />
+
+    </Fragment>
   );
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = ( {date_now} ) => (
+        <footer>
+            <p>Todos los derechos reservados &copy; {date_now}</p>
+        </footer>
+      );
+
+ export default Footer;
+
